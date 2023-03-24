57a11f30f12a5e55ef14396e30b344bc665c70f4
souravlayek <souravlayek11@gmail.com>
2023-03-24 17:40:54 +0530

    fixed temp file issue

--- internal/api/router/router.go
+++ internal/api/router/router.go
@@ -9,11 +9,10 @@ import (
 
 func CreateRouter() *mux.Router {
 	fs := http.FileServer(http.Dir("media"))
+	fs2 := http.FileServer(http.Dir("media"))
 	http.Handle("/media/", http.StripPrefix("/media/", fs))
+	http.Handle("/temp/", http.StripPrefix("/temp/", fs2))
 	router := mux.NewRouter()
-	router.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "media/test.jpg")
-	})
 	router.HandleFunc("/api/upload", handler.UploadFileHandler).Methods("POST")
 	router.HandleFunc("/s/{id}", handler.DownloadFile).Methods("GET")
 	return router
